02db6ff9dcc67a03248eaa194a5e78ac29fdf5c6
Greg Doud <gpdoud@gmail.com>
2019-02-20 07:29:50 -0500

    Add project files.

---
+++ LinqExtensionMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LinqExtensionMethods {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
